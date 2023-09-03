0dca39ef9a684a8f3b272192c9c47f6e2cfba2b7
amiranga <ae.amunugama@gmail.com>
2023-09-03 21:09:45 +0400

    Fixed some issues

--- src/main/java/com/cs/datastructures/Algorithms.java
+++ src/main/java/com/cs/datastructures/Algorithms.java
@@ -20,36 +20,36 @@ public class Algorithms {
     }
 
 
-    public static int recursiveMaxWeight(int[] nums) {
-        return recursiveMaxWeight(nums, 0, nums.length - 1);
+    public static int recursiveMaxWeight(int[] array) {
+        return recursiveMaxWeight(array, 0, array.length - 1);
     }
 
-    private static int recursiveMaxWeight(int[] nums, int i, int j) {
+    private static int recursiveMaxWeight(int[] array, int i, int j) {
         if (i == j) {
-            return nums[i];
+            return array[i];
         }
 
         int mid = (i + j) / 2;
-        int sum = 0, leftMaxSUM = 0;
+        int sum = 0, leftMaxSum = 0;
 
         for (int l = mid; l >= i; l--) {
-            sum += nums[l];
-            if (sum > leftMaxSUM) {
-                leftMaxSUM = sum;
+            sum += array[l];
+            if (sum > leftMaxSum) {
+                leftMaxSum = sum;
             }
         }
 
-        int rightMaxSUM = 0;
+        int rightMaxSum = 0;
         sum = 0;
         for (int l = mid + 1; l <= j; l++) {
-            sum += nums[l];
-            if (sum > rightMaxSUM) {
-                rightMaxSUM = sum;
+            sum += array[l];
+            if (sum > rightMaxSum) {
+                rightMaxSum = sum;
             }
         }
 
-        int maxLeftRight = Math.max(recursiveMaxWeight(nums, i, mid), recursiveMaxWeight(nums, mid + 1, j));
-        return Math.max(maxLeftRight, leftMaxSUM + rightMaxSUM);
+        int maxLeftRight = Math.max(recursiveMaxWeight(array, i, mid), recursiveMaxWeight(array, mid + 1, j));
+        return Math.max(maxLeftRight, leftMaxSum + rightMaxSum);
     }
 
     public static int optimalMaxWeight(int[] nums) {
